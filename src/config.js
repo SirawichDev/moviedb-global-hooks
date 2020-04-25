8b568968a40f0397b16463d594ef96fb985901b6
SirawichDev <sirawit0676@gmail.com>
2020-04-25 13:25:04 +0700

    complete:sparkler:

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
+
 const API_URL = "https://api.themoviedb.org/3/";
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
 const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 
 const BACKDROP_SIZE = "w1280";
 
 const POSTER_SIZE = "w500";
 
-export { API_KEY, API_URL, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
+module.exports = { API_KEY, API_URL, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };

65c3196883eb43d5afeee0c194c4151f0d12f567
Sirawich Vongchuy <sirawichvongchuy@Sirawichs-MacBook-Pro.local>
2020-04-28 06:28:54 +0700

    HeaderImage set

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
 import React, { useContext } from "react";
-
 import { MoiveContext } from "../App";
+import Header from "./elements/Header";
+import HeaderImage from "./elements/HeaderImage";
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import Spinner from "./elements/Spinner";
 const Home = () => {
   const movieContext = useContext(MoiveContext);
-  console.log(movieContext.movieState);
-  return <h1>asds</h1>;
+  const { headerImage } = movieContext.movieState;
+  if (movieContext.error) return <div>Something Wrong...</div>;
+  if (!headerImage) return <Spinner />;
+  return (
+    <>
+      <Header />
+      <HeaderImage
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${headerImage.backdrop_path}`}
+        title={headerImage.original_title}
+        text={headerImage.overview}
+      />
+      <Spinner/>
+      <GlobalStyle/>
+    </>
+  );
 };
 
 export default Home;

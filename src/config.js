7825a4ab2093c79e88d4462aef5f398e2d608796
Sirawich Vongchuy <sirawichvongchuy@Sirawichs-MacBook-Pro.local>
2020-04-28 06:30:28 +0700

    process.env key added

--- src/config.js
+++ src/config.js
@@ -6,5 +6,4 @@ const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 const BACKDROP_SIZE = "w1280";
 
 const POSTER_SIZE = "w500";
-
 module.exports = { API_KEY, API_URL, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };

57bb6d21565d478e88d07e9723daa9c09b433d63
Sirawich Vongchuy <sirawichvongchuy@Sirawichs-MacBook-Pro.local>
2020-04-29 14:22:37 +0700

    .

--- src/components/elements/Grid.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { StyledGrid } from '../styles/StyledGrid'
-const Grid = ({ children }) => {
-  return <StyledGrid>{children}</StyledGrid>;
-};
-
-export default Grid;

870c43d45a5f4e384cd6e359055dedcfba58b99e
Sirawich Vongchuy <sirawichvongchuy@Sirawichs-MacBook-Pro.local>
2020-04-29 14:22:56 +0700

    Header Styled set

--- src/components/styles/StyledHeader.js
+++ src/components/styles/StyledHeader.js
@@ -5,7 +5,25 @@ export const StyledHeader = styled.div`
 
   .header-content {
     max-width: 1280px;
-    min-height: 120px;
+    background: linear-gradient(
+        to bottom,
+        #d5dee7 0%,
+        #e8ebf2 50%,
+        #e2e7ed 100%
+      ),
+      linear-gradient(
+        to bottom,
+        rgba(0, 0, 0, 0.02) 50%,
+        rgba(255, 255, 255, 0.02) 61%,
+        rgba(0, 0, 0, 0.02) 73%
+      ),
+      linear-gradient(
+        33deg,
+        rgba(255, 255, 255, 0.2) 0%,
+        rgba(0, 0, 0, 0.2) 100%
+      );
+    background-blend-mode: normal, color-burn;
+    min-height: 100px;
     margin: 0 auto;
     box-sizing: border-box;
 
@@ -17,11 +35,11 @@ export const StyledHeader = styled.div`
 
 export const StyledMeLogo = styled.img`
   width: 200px;
-  position:absolute;
+  position: absolute;
   top: -50px;
-  left:0;
+  left: 0;
 
-  @media screen and(max-width: 500px){
+  @media screen and(max-width: 500px) {
     display: inline-block;
     width: 80px;
     margin-top: 5px;

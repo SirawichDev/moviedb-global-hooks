bdc5d05a0007fdc8554e4e80de1aca026284f95a
Sirawich Vongchuy <sirawichvongchuy@Sirawichs-MacBook-Pro.local>
2020-04-29 14:25:42 +0700

    MovieThumb Styled set

---
+++ src/components/styles/StyledMovieThumb.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+
+
+export const StyledMovieThumb = styled.div`
+img{
+    margin: 20px;
+    width:200px;
+    border-radius: 20px;
+    box-shadow: 0 2px 5px rgba(0,0,0,0.1), 
+    0 2px 2px rgba(0,0,0,0.1), 
+    0 4px 4px rgba(0,0,0,0.1), 
+    0 8px 8px rgba(0,0,0,0.1),
+    0 16px 16px rgba(0,0,0,0.1);
+    margin: 20px;
+
+}
+
+`

53f992e6256473d5f119801d59fc179966bdc9fc
Sirawich Vongchuy <sirawichvongchuy@Sirawichs-MacBook-Pro.local>
2020-04-29 14:20:25 +0700

    prepare searchTerma

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MoiveContext } from "../App";
 import Header from "./elements/Header";
+import Spinner from "./elements/Spinner";
+import Grid from "./elements/Grid";
+import MovieThumb from "./elements/MovieThumb";
 import HeaderImage from "./elements/HeaderImage";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import { GlobalStyle } from "./styles/GlobalStyle";
-import Spinner from "./elements/Spinner";
+
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const movieContext = useContext(MoiveContext);
   const { headerImage } = movieContext.movieState;
+  console.log(movieContext.movieState.movies.map((m) => m.poster_path));
   if (movieContext.error) return <div>Something Wrong...</div>;
   if (!headerImage) return <Spinner />;
   return (
@@ -18,8 +23,23 @@ const Home = () => {
         title={headerImage.original_title}
         text={headerImage.overview}
       />
-      <Spinner/>
-      <GlobalStyle/>
+      <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
+        {movieContext.movieState.movies.map((movie) => (
+          <MovieThumb
+            key={movie.id}
+            image={
+              movie.poster_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                : "No Image Found"
+            }
+            movieId={movie.id}
+            movieName={movie.original_title}
+            clickable
+          />
+        ))}
+      </Grid>
+      <Spinner />
+      <GlobalStyle />
     </>
   );
 };

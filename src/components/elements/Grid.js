b007325ef2edd71e5974c3e529a3ddbe27ae3e21
Sirawich Vongchuy <sirawichvongchuy@Sirawichs-MacBook-Pro.local>
2020-04-29 14:21:27 +0700

    flex hoc set

---
+++ src/components/elements/Grid.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { StyledGrid } from '../styles/StyledGrid'
+const Grid = ({ children }) => {
+  return <StyledGrid>{children}</StyledGrid>;
+};
+
+export default Grid;

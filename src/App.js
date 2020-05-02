91170ca07467983fbea8ccaedcb11e88108f3eec
SirawichDev <sirawit0676@gmail.com>
2020-05-02 11:28:34 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
 import React, { useReducer, useEffect } from "react";
-import "./App.css";
 import Home from "./components/Home";
-import {
-  API_KEY,
-  API_URL,
-} from "./config";
+import { MoviesReducer } from "./components/movieReducers/movieReducer";
+import { API_KEY, API_URL, SEARCH_BASE_URL ,POPULAR_BASE_URL} from "./config";
 
 export const MoiveContext = React.createContext();
 const initState = {
   movies: [{}],
   loading: true,
+  currentPage: 0,
   headerImage: {},
-  error: ""
+  totalPages: 0,
+  searchTerm: "",
+  error: "",
 };
 
-const MoviesReducer = (state, action) => {
-  switch (action.type) {
-    case "FETCH_SUCCESS":
-      return { ...state, movies: action.payload,headerImage: action.headerImage , loading: false };
-
-    case "FETCH_ERR":
-      return { loading: false, error: action.error };
-
-    default:
-      return state;
-  }
-};
 function App() {
-  const [movieState, dispatch] = useReducer(
-    MoviesReducer,
-    initState
-  );
+  const [movieState, dispatch] = useReducer(MoviesReducer, initState);
   const fetchMovies = async (endpoint) => {
     try {
       const result = await (await fetch(endpoint)).json();
-      dispatch({ type: "FETCH_SUCCESS", payload: [...result.results],headerImage: result.results[0] });
+      dispatch({
+        type: "FETCH_SUCCESS",
+        payload: result,
+        isLoadMore: endpoint.search("page"),
+      });
     } catch (error) {
       dispatch({ type: "FETCH_ERR", error: error });
     }
+  };
+
+  const loadMore = () => {
+    const { currentPage, searchTerm } = movieState;
+
+    const searchEndpoint = `${SEARCH_BASE_URL}${searchTerm}&page=${
+      currentPage + 1
+    }`;
+    const popularEndpoint = `${POPULAR_BASE_URL}&page=${currentPage + 1}`;
+
+    const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
+
+    fetchMovies(endpoint);
   };
   useEffect(() => {
     fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
   }, []);
   return (
-    <MoiveContext.Provider value={{ movieState: movieState, movieDispath: dispatch }}>
-        <Home />
+    <MoiveContext.Provider
+      value={{
+        movieState: movieState,
+        movieDispath: dispatch,
+        loadmoreFn: loadMore,
+      }}
+    >
+      <Home />
     </MoiveContext.Provider>
   );
 }

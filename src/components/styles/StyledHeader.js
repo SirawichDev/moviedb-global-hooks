c6837b795f8e6b8def11bfbe7d93838938a6f61e
SirawichDev <sirawit0676@gmail.com>
2020-05-02 11:30:38 +0700

    logo position change

--- src/components/styles/StyledHeader.js
+++ src/components/styles/StyledHeader.js
@@ -2,27 +2,8 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.div`
   box-sizing: border-box;
-
   .header-content {
     max-width: 1280px;
-    background: linear-gradient(
-        to bottom,
-        #d5dee7 0%,
-        #e8ebf2 50%,
-        #e2e7ed 100%
-      ),
-      linear-gradient(
-        to bottom,
-        rgba(0, 0, 0, 0.02) 50%,
-        rgba(255, 255, 255, 0.02) 61%,
-        rgba(0, 0, 0, 0.02) 73%
-      ),
-      linear-gradient(
-        33deg,
-        rgba(255, 255, 255, 0.2) 0%,
-        rgba(0, 0, 0, 0.2) 100%
-      );
-    background-blend-mode: normal, color-burn;
     min-height: 100px;
     margin: 0 auto;
     box-sizing: border-box;
@@ -36,9 +17,11 @@ export const StyledHeader = styled.div`
 export const StyledMeLogo = styled.img`
   width: 200px;
   position: absolute;
-  top: -50px;
-  left: 0;
-
+  top: 4%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+  -webkit-backface-visibility: hidden; 
   @media screen and(max-width: 500px) {
     display: inline-block;
     width: 80px;

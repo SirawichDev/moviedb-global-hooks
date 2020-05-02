6dbae9008045f5ea8aad3b211fa0247e01a45cda
Sirawich Vongchuy <sirawichvongchuy@Sirawichs-MacBook-Pro.local>
2020-05-02 11:26:28 +0700

    loadmoer

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { MoiveContext } from "../App";
 import Header from "./elements/Header";
 import Spinner from "./elements/Spinner";
 import Flex from "./elements/Flex";
+import LoadMore from "./elements/LoadMore";
 import MovieThumb from "./elements/MovieThumb";
 import HeaderImage from "./elements/HeaderImage";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
@@ -11,8 +12,13 @@ import { GlobalStyle } from "./styles/GlobalStyle";
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const movieContext = useContext(MoiveContext);
-  const { headerImage } = movieContext.movieState;
-  console.log(movieContext.movieState.movies.map((m) => m.poster_path));
+  const {
+    headerImage,
+    loading,
+    currentPage,
+    totalPages,
+  } = movieContext.movieState;
+  console.log(movieContext);
   if (movieContext.error) return <div>Something Wrong...</div>;
   if (!headerImage) return <Spinner />;
   return (
@@ -38,7 +44,10 @@ const Home = () => {
           />
         ))}
       </Flex>
-      <Spinner />
+      {loading && <Spinner />}
+      {currentPage < totalPages && !loading && (
+        <LoadMore text="Load more" callback={movieContext.loadmoreFn} />
+      )}
       <GlobalStyle />
     </>
   );

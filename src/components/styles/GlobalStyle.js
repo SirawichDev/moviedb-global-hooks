708bd8e8de14e67f2959520a725d847887fb9585
SirawichDev <sirawit0676@gmail.com>
2020-05-02 11:29:33 +0700

    bg change

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -6,5 +6,23 @@ body{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background: linear-gradient(
+        to bottom,
+        #d5dee7 0%,
+        #e8ebf2 50%,
+        #e2e7ed 100%
+      ),
+      linear-gradient(
+        to bottom,
+        rgba(0, 0, 0, 0.02) 50%,
+        rgba(255, 255, 255, 0.02) 61%,
+        rgba(0, 0, 0, 0.02) 73%
+      ),
+      linear-gradient(
+        33deg,
+        rgba(255, 255, 255, 0.2) 0%,
+        rgba(0, 0, 0, 0.2) 100%
+      );
+    background-blend-mode: normal, color-burn;
 }
 `
